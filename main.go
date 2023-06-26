e99a09a014f50284295960165d3fc07ee2a3e7e8
Enis Arik <enis.arik@sap.com>
2023-06-26 22:59:33 +0300

    First commit.

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "github.com/gocolly/colly"
+)
+
+func scrape() {
+    c := colly.NewCollector()
+
+	c.OnHTML("div.text-xl.font-semibold.text-white", func(e *colly.HTMLElement) {
+		fmt.Println("Dolar/TRY Paritesi:", e.Text)
+	})
+
+    c.Visit("https://kur.doviz.com/serbest-piyasa/amerikan-dolari")
+}
+
+func main() {
+    scrape()
+}
