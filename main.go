a61bd727702821531e02582e3a1bac3bf9bd8c7c
Enis Arik <enis.arik@sap.com>
2023-06-27 11:42:29 +0300

    fixed indentation

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func scrape() {
     c := colly.NewCollector()
 
     c.OnHTML("div.text-xl.font-semibold.text-white", func(e *colly.HTMLElement) {
-		fmt.Println("Dolar/TRY Paritesi:", e.Text)
+	    fmt.Println("Dolar/TRY Paritesi:", e.Text)
 	})
 
     c.Visit("https://kur.doviz.com/serbest-piyasa/amerikan-dolari")
